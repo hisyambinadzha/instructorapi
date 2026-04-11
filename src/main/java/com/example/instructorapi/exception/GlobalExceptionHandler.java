0fc0039b06c68cdb0b2b32c161a6f6ccf194bcc5
DESKTOP-FKIUNFT\Admin <hisyamadzha@gmail.com>
2026-04-11 16:08:48 +0800

    Added exersice 5 progress

---
+++ src/main/java/com/example/instructorapi/exception/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.example.instructorapi.exception;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public  Map<String, Object> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            errors.put(error.getField(), error.getDefaultMessage());
+        });
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("errors", "Validation failed");
+        response.put("details", errors);
+
+        return response;
+    }
+}
