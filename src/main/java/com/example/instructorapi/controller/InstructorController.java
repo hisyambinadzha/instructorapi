ab40875e1eafe4eab5c84806848694579bdefc8e
DESKTOP-FKIUNFT\Admin <hisyamadzha@gmail.com>
2026-04-19 11:48:09 +0800

    Code clean up

--- src/main/java/com/example/instructorapi/controller/InstructorController.java
+++ src/main/java/com/example/instructorapi/controller/InstructorController.java
@@ -1,8 +1,6 @@
 package com.example.instructorapi.controller;
 
 import java.util.List;
-import java.util.Map;
-
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
