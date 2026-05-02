f960e131e246179af4aa78415355af69562c7687
DESKTOP-FKIUNFT\Admin <hisyamadzha@gmail.com>
2026-05-02 11:09:16 +0800

    Code clean up

--- src/main/java/com/example/instructorapi/repository/InstructorRepository.java
+++ src/main/java/com/example/instructorapi/repository/InstructorRepository.java
@@ -1,12 +1,9 @@
 package com.example.instructorapi.repository;
 
 import java.util.List;
-import java.util.Optional;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.instructorapi.model.Instructor;
